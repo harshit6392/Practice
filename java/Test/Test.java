5a65867fc1b22f60de03fd5a445fe3376c6d0932
Harshit <hsharma6392@gmail.com>
2024-02-17 23:53:52 +0530

    Practice

--- java/Test/Test.java
+++ java/Test/Test.java
@@ -1,7 +1,19 @@
 package Test;
+
+
 public class Test{
-    
+    public static void fun(int arr[]){
+        int num=2;
+        for(int i=0;i<arr.length;i++){
+            if (arr[i]==num) {
+                arr[i]=arr[i+1];
+            }
+            System.out.println(i +" " + arr[i]);
+        }
+
+    }    
     public static void main(String[] args) {
-        
+        int arr[]={2,1,3,4,5};
+        fun(arr);
     }
 }
