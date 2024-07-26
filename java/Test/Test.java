dc5d7df0252a416c5cc38d6ad525fd80c4511d65
Harshit <hsharma6392@gmail.com>
2024-07-27 02:12:48 +0530

    Learning Recursion

--- java/Test/Test.java
+++ java/Test/Test.java
@@ -1,7 +1,28 @@
 package Test;
 
 public class Test{
+    public static void check(int arr[][]){
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(arr[i][j]==0){
+                    for(int k=i;k<n;k++){
+                        for(int l=j;l<n;l++){
+                            arr[k][l]=0;
+                        }
+                    }
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-        
+        int arr[][]={{1,0,1},{1,1,1},{1,1,1}};
+        check(arr);
     }
 }
