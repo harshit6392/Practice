31cac86ee7046229d4727e14a561457dc2652ab1
Harshit <hsharma6392@gmail.com>
2024-07-27 13:47:11 +0530

    Learning Recursion

--- java/Recursion/FirstOccurence.java
+++ java/Recursion/FirstOccurence.java
@@ -1,11 +1,14 @@
 package Recursion;
 
+//finding the first occurence of a number in the array using recursion
 public class FirstOccurence {
     public static int searchOccurence(int arr[], int key, int i){
         if(i==arr.length)
         return -1;
+        // Base Case
         if(arr[i]==key)
         return i;
+        // recursive call
         return searchOccurence(arr, key, i+1);
     }
     public static void main(String[] args) {
